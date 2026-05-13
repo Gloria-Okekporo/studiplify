2acc89e7ee3577aa012ecd83277a690f1a24e385
Gloria-Okekporo <tejiriokes@gmail.com>
2026-05-13 13:52:25 +0100

    Fix quiz page

--- app/dashboard/quiz/page.tsx
+++ app/dashboard/quiz/page.tsx
@@ -1,6 +1,6 @@
-import QuizClient from '@/components/Dashboard/QuizClient';
-import { getUserQuizzes } from '@/lib/actions/quizzes';
 import { redirect } from 'next/navigation';
+import QuizClient from './QuizClient';
+import { getUserQuizzes } from '@/lib/actions/quiz.actions';
 
 export const metadata = {
   title: 'AI Quizzes - Studiplify',
@@ -9,13 +9,17 @@ export const metadata = {
 export default async function QuizPage() {
   try {
     const quizRes = await getUserQuizzes();
-    const quizzes = quizRes?.success ? (quizRes?.data || []) : [];
+
+    const quizzes = quizRes?.success
+      ? (quizRes?.data || [])
+      : [];
 
     return <QuizClient initialQuizzes={quizzes} />;
   } catch (error: any) {
     if (error.message === 'Unauthorized') {
       redirect('/auth/login');
     }
+
     return <QuizClient initialQuizzes={[]} />;
   }
 }
